ee0e5a72bc93a0da37f0abac3e1f98cf0a7b7f01
zichen <17512552973@163.com>
2021-03-09 18:48:45 +0800

    remove /out from remote

--- out/_next/static/chunks/pages/ExChange/animatedIcons/TwoWay-b09a6a80ee53ab4af9fc.js
+++
@@ -1,1 +0,0 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[15],{"2DuL":function(n,_,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/ExChange/animatedIcons/TwoWay",function(){return w("s19+")}])}},[["2DuL",0,1,2]]]);
