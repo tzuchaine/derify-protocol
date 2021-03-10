595ab2084412ba54abd51acf5234abcd4c79d5bd
zichen <17512552973@163.com>
2021-03-10 10:39:48 +0800

    add background animate

--- common/components/AnimateBG.tsx
+++ common/components/AnimateBG.tsx
@@ -25,7 +25,7 @@ class AnimateBG extends React.Component {
         var elOffsetWidth = el.offsetWidth - pad;
         var parentOffsetWidth = parentEl.offsetWidth;
         var ratio = parentOffsetWidth / elOffsetWidth;
-        timeout = setTimeout(() => anime.set(el, { scale: ratio }), 10);
+        timeout = setTimeout(() => anime.set(el, { scale: ratio  }), 100);
       }
       resize();
       window.addEventListener("resize", resize);
@@ -35,8 +35,8 @@ class AnimateBG extends React.Component {
       var staggerVisualizerEl = document.querySelector(".stagger-visualizer");
       var dotsWrapperEl = staggerVisualizerEl.querySelector(".dots-wrapper");
       var dotsFragment = document.createDocumentFragment();
-      var grid = [28, 10];
-      var cell = 66;
+      var grid = [25, 15];
+      // var cell = 60;
       var numberOfElements = grid[0] * grid[1];
       var animation;
       var paused = true;
@@ -46,7 +46,7 @@ class AnimateBG extends React.Component {
       for (var i = 0; i < numberOfElements; i++) {
         var dotEl = document.createElement("div");
         dotEl.classList.add("dot");
-        dotEl.classList.add(styles.dot);;
+        dotEl.classList.add(styles.dot);
         dotsFragment.appendChild(dotEl);
       }
 
@@ -55,20 +55,20 @@ class AnimateBG extends React.Component {
       var index = anime.random(0, numberOfElements - 1);
       var nextIndex = 0;
 
-      anime.set(".stagger-visualizer .cursor", {
-        translateX: anime.stagger(-cell, {
-          grid: grid,
-          from: index,
-          axis: "x",
-        }),
-        translateY: anime.stagger(-cell, {
-          grid: grid,
-          from: index,
-          axis: "y",
-        }),
-        translateZ: 0,
-        scale: 1.5,
-      });
+      // anime.set(".stagger-visualizer .cursor", {
+      //   translateX: anime.stagger(-cell, {
+      //     grid: grid,
+      //     from: index,
+      //     axis: "x",
+      //   }),
+      //   translateY: anime.stagger(-cell, {
+      //     grid: grid,
+      //     from: index,
+      //     axis: "y",
+      //   }),
+      //   translateZ: 0,
+      //   scale: 1.5,
+      // });
 
       function play() {
         paused = false;
@@ -81,26 +81,27 @@ class AnimateBG extends React.Component {
             easing: "easeInOutQuad",
             complete: play,
           })
-          .add({
-            targets: ".stagger-visualizer .cursor",
-            keyframes: [
-              { scale: 0.75, duration: 120 },
-              { scale: 1.5, duration: 220 },
-              { scale: 1.2, duration: 450 },
-            ],
-            duration: 300,
-          })
+          // .add({
+          //   targets: ".stagger-visualizer .cursor",
+          //   keyframes: [
+          //     { scale: 0.75, duration: 120 },
+          //     { scale: 2.5, duration: 220 },
+          //     { scale: 1.5, duration: 450 },
+          //   ],
+          //   duration: 300,
+          // })
+
           .add(
             {
               targets: ".stagger-visualizer .dot",
               keyframes: [
                 {
-                  translateX: anime.stagger("-2px", {
+                  translateX: anime.stagger("-1px", {
                     grid: grid,
                     from: index,
                     axis: "x",
                   }),
-                  translateY: anime.stagger("-2px", {
+                  translateY: anime.stagger("-1px", {
                     grid: grid,
                     from: index,
                     axis: "y",
@@ -108,17 +109,17 @@ class AnimateBG extends React.Component {
                   duration: 100,
                 },
                 {
-                  translateX: anime.stagger("4px", {
+                  translateX: anime.stagger("1px", {
                     grid: grid,
                     from: index,
                     axis: "x",
                   }),
-                  translateY: anime.stagger("4px", {
+                  translateY: anime.stagger("1px", {
                     grid: grid,
                     from: index,
                     axis: "y",
                   }),
-                  scale: anime.stagger([1.2, 1], { grid: grid, from: index }),
+                  scale: anime.stagger([1.5, 1], { grid: grid, from: index }),
                   duration: 225,
                 },
                 {
@@ -128,32 +129,35 @@ class AnimateBG extends React.Component {
                   duration: 1200,
                 },
               ],
-              delay: anime.stagger(80, { grid: grid, from: index }),
+              delay: anime.stagger(100, { grid: grid, from: index }),
             },
             30
           )
-          .add(
-            {
-              targets: ".stagger-visualizer .cursor",
-              translateX: {
-                value: anime.stagger(-cell, {
-                  grid: grid,
-                  from: nextIndex,
-                  axis: "x",
-                }),
-              },
-              translateY: {
-                value: anime.stagger(-cell, {
-                  grid: grid,
-                  from: nextIndex,
-                  axis: "y",
-                }),
-              },
-              scale: 1.5,
-              easing: "cubicBezier(.075, .2, .165, 1)",
-            },
-            "-=800"
-          );
+          // move cursor
+          // .add(
+          //   {
+          //     targets: ".stagger-visualizer .cursor",
+          //     //  moving along the X-axis
+          //     translateX: {
+          //       value: anime.stagger(-cell, {
+          //         grid: grid,
+          //         from: nextIndex,
+          //         axis: "x",
+          //       }),
+          //     },
+          //     // moving along the Y-axis
+          //     translateY: {
+          //       value: anime.stagger(-cell, {
+          //         grid: grid,
+          //         from: nextIndex,
+          //         axis: "y",
+          //       }),
+          //     },
+          //     scale: 1.5,
+          //     easing: "cubicBezier(.075, .2, .165, 1)",
+          //   },
+          //   "-=1200"
+          // );
 
         index = nextIndex;
       }
